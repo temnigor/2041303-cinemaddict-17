4e4ca803551813c451efe316e52bfcd78fe4995d
Nikita <temnigor@gmail.com>
2022-05-05 03:43:59 +0400

    изменил ивент на более лаконичное написание

--- src/model/film-card-model.js
+++ src/model/film-card-model.js
@@ -1,6 +1,6 @@
 import { getSomeFilm } from './fish-film-card.js';
 export default class NewfilmCardModel {
-  #films = Array.from({length:0}, getSomeFilm);
+  #films = Array.from({length:25}, getSomeFilm);
   get films  () {
     return this.#films;
   }
--- src/prisenter/popup-prisenter.js
+++ src/prisenter/popup-prisenter.js
@@ -1,55 +1,55 @@
 
 import NewPopup from '../view/popup.js';
 import { render } from '../render.js';
-const getNeedComment = (allFilmComments, filmsModel) => {
-  const keyFilmsComments = filmsModel.comments;
-  const needComments = [];
-  keyFilmsComments.forEach((oneKey)=>{
-    for(const comment of allFilmComments){
-      if(oneKey === Number(comment.id)){
-        needComments.push(comment);
-      }
-    }
-  });
-  return needComments;
-};
-
-const getEventClouse = (popup, filmContener) => {
-  popup.element.querySelector('.film-details__close-btn').addEventListener('click', (evt) =>{
-    evt.preventDefault();
-    removeElementAndEvent(popup, filmContener);
-  });
-  document.addEventListener('keydown', (evt)=>{
-    if(evt.code === 'Escape') {
-      removeElementAndEvent(popup, filmContener);
-    }
-  });
-};
-function removeElementAndEvent (popup, filmContener) {
-  filmContener.removeChild(popup.element);
-  filmContener.classList.remove('hide-overflow');
-  document.removeEventListener('keydown', getEventClouse);
-}
-
 export default class NewFilmPopup {
   #filmsContainer = null;
   #filmCardModel = null;
   #filmComments = null;
   #allFilmComment = [];
   #filmComment =null;
+  #popup = null;
   init = (filmContener, filmCardModel, filmComment ) => {
     this.#filmsContainer = filmContener;
     this.#filmCardModel = filmCardModel;
     this.#filmComments = filmComment;
     this.#allFilmComment = [...this.#filmComments.comments];
-    this.#filmComment = getNeedComment(this.#allFilmComment, this.#filmCardModel);
-
-    this.#renderPopup(this.#filmCardModel, this.#filmComment);
+    this.#filmComment = this.#getNeedComment(this.#allFilmComment, this.#filmCardModel);
+    this.#popup = new NewPopup( this.#filmCardModel, this.#filmComment);
+    render (this.#popup, this.#filmsContainer);
+    this.#getEventClouse();
   };
 
-  #renderPopup = (filmModel, filmComment) => {
-    const popup = new NewPopup(filmModel, filmComment);
-    render (popup, this.#filmsContainer);
-    getEventClouse(popup, this.#filmsContainer);
+  #getNeedComment = (allFilmComments, filmsModel) => {
+    const keyFilmsComments = filmsModel.comments;
+    const needComments = [];
+    keyFilmsComments.forEach((oneKey)=>{
+      for(const comment of allFilmComments){
+        if(oneKey === Number(comment.id)){
+          needComments.push(comment);
+        }
+      }
+    });
+    return needComments;
+  };
+
+  #getEventClouse = () => {
+    this.#popup.element.querySelector('.film-details__close-btn').addEventListener('click', (evt) =>{
+      evt.preventDefault();
+      this.#removeElementAndEvent();
+    });
+    document.addEventListener('keydown',this.#findKey);
+  };
+
+  #findKey = (evt) => {
+    if(evt.code === 'Escape'){
+      evt.preventDefault();
+      this.#removeElementAndEvent();
+    }
+  };
+
+  #removeElementAndEvent = () => {
+    this.#filmsContainer.removeChild(this.#popup.element);
+    this.#filmsContainer.classList.remove('hide-overflow');
+    document.removeEventListener('keydown', this.#findKey);
   };
 }
