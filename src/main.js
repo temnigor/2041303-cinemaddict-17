61d6d729e8e01ffa54eb54e5c71f67b575c047c6
Nikita <temnigor@gmail.com>
2022-06-23 15:49:17 +0400

    Удаление пока не сделанных кусков допа

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import AvatarRankPresenter from './presenter/avatar-rank-presenter.js';
 import { render } from './framework/render.js';
 import FilmList from './view/film-list.js';
-import FilmsRated from './view/films-rated.js';
-import FilmsMostComment from './view/films-most-commented.js';
 import FilmsCatalogPresenter from './presenter/films-catalog-presenter.js';
-import RatedFilmsPresenter from './presenter/rated-films-presenter.js';
-import MostCommentFilmsPresenter from './presenter/most-comment-films-presenter.js';
 import FilmCardModel from './model/film-card-model.js';
 import FilmCommentModel from './model/film-comment-model.js';
 import Api from './model/api-module.js';
@@ -16,22 +12,13 @@ const header = document.querySelector('.header');
 const avatarRankPresenter = new AvatarRankPresenter(header);
 const main = document.querySelector('.main');
 const body = document.querySelector('body');
-const filmsCatalogRatedPresenter = new RatedFilmsPresenter();
-const filmsCatalogMostCommentPresenter = new MostCommentFilmsPresenter();
 const api = new Api(END_POINT, AUTHORIZATION);
 const filmsCardModel = new FilmCardModel(api);
 const filmsComment = new FilmCommentModel (api);
+filmsCardModel.init();
 render(new FilmList(), main);
 const filmsContainer = main.querySelector('.films-list__container');
-filmsCardModel.init();
 const filmsCatalogPresenter = new FilmsCatalogPresenter(filmsContainer, filmsCardModel, body, filmsComment, avatarRankPresenter);
 filmsCatalogPresenter.init();
-const films = main.querySelector('.films');
-render(new FilmsRated, films);
-render(new FilmsMostComment, films);
-const topRated = main.querySelector('.rated');
-filmsCatalogRatedPresenter.init(topRated, filmsCardModel);
-const mostCommented = main.querySelector('.most_commented');
-filmsCatalogMostCommentPresenter.init(mostCommented, filmsCardModel);
 
 
